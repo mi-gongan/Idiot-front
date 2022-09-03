61e83a524f4c91340cd6edffa8a59b86479711db
INBUM02 <kib0202@naver.com>
2022-09-04 01:08:41 +0900

    FEAT: window객체에 ethereum 타입을 any로 지정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,12 @@ import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import Layout from "../src/components/Layout";
 
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
